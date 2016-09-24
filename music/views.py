0e39dc357d5eb3799b4228c314940045986c9275
Fikri Abdul <fikri.abdul@gmx.com>
2016-09-24 09:50:52 +0700

    change HttpResponse to Render

--- music/views.py
+++ music/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+from django.http import Http404
+
+# from django.template import loader
 
 from .models import Album
 
 # Create your views here.
 def index(request):
     all_albums = Album.objects.all()
-    template= loader.get_template('music/index.html')
-    context = {
-        'all_albums': all_albums,
-    }
-
-    return HttpResponse(template.render(context, request))
+    # template= loader.get_template('music/index.html')
+    
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'music/index.html', {'all_albums': all_albums,})
 
     # html = ""
     # for album in all_albums:
@@ -21,6 +21,12 @@ def index(request):
     # return HttpResponse(html)
 
 def detail(request, album_id):
-    return HttpResponse('<h2>Details for album id:'+str(album_id)+' </h2>')
+    # return HttpResponse('<h2>Details for album id:'+str(album_id)+' </h2>')
+    try:
+        album = Album.objects.get(pk=album_id)
+    except  Album.DoesNotExist:
+        raise Http404("Album doesnot exist")
+
+    return render(request, 'music/detail.html', {'album': album,})
 
 
