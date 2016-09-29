45d4c3fb716f6b87fb1d46d7cefcdbec84374bf6
Fikri Abdul <fikri.abdul@gmx.com>
2016-09-29 09:49:28 +0700

    using generic of view

--- music/views.py
+++ music/views.py
@@ -1,33 +1,13 @@
-from django.shortcuts import render, get_object_or_404
-# from django.http import HttpResponse
-# from django.http import Http404
-
-# from django.template import loader
-
+from django.views import generic
 from .models import Album
 
-# Create your views here.
-def index(request):
-    all_albums = Album.objects.all()
-    # template= loader.get_template('music/index.html')
-    
-    # return HttpResponse(template.render(context, request))
-    return render(request, 'music/index.html', {'all_albums': all_albums,})
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'all_albums'
 
-    # html = ""
-    # for album in all_albums:
-    #     url ='/music/'+str(album.id)+'/'
-    #     html += '<a href=" ' + url + '">'+album.album_title+'</a><br/>'
-    # return HttpResponse(html)
+    def get_queryset(self):
+        return Album.objects.all()
 
-def detail(request, album_id):
-    # return HttpResponse('<h2>Details for album id:'+str(album_id)+' </h2>')
-    # try:
-    #     album = Album.objects.get(pk=album_id)
-    # except  Album.DoesNotExist:
-    #     raise Http404("Album doesnot exist")
-
-    album = get_object_or_404(Album, pk=album_id)
-    return render(request, 'music/detail.html', {'album': album,})
-
-
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
